60be7b972649821beee9baf55a2b1ac14127e919
andreeaCcss <andreeaccss@gmail.com>
2019-08-26 15:22:28 +0200

    Fetched Data

--- src/components/Pages/DogList/DogList.js
+++ src/components/Pages/DogList/DogList.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import Logo from "../../Layout/Logo/Logo"
-import BackArrow from "../../Layout/BackArrow/BackArrow"
+import Logo from "../../Layout/Logo/Logo";
+import BackArrow from "../../Layout/BackArrow/BackArrow";
 
 class DogList extends Component {
-	render() {
-		return (
-			<div>
-				<header>
-					<Logo />
-					<BackArrow />
-				</header>
-				<main>
-                    <Link to="/dog-list/labrador">Dog detail</Link>
-				</main>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <header>
+          <Logo />
+          <BackArrow />
+        </header>
+        <main>
+          <Link to="/dog-list/labrador">Dog detail</Link>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default DogList;
