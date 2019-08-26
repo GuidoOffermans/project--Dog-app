82d55d7870628acb8d1c355250fbf06e911e60da
msbb <mail@maximbuur.nl>
2019-08-26 13:39:48 +0200

    Worked on components

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import './layout.css';
+import './PageContainer.css';
 import { Route, Switch } from "react-router-dom";
 
-
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import HomePage from '../HomePage/HomePage'
+// import Footer from '../Footer/Footer';
+// import Header from '../Header/Header';
+import HomePage from '../Pages/HomePage/HomePage'
+import Games from "../Pages/Games/Games"
+import DogList from "../Pages/DogList/DogList"
+import DogDetail from "../Pages/DogDetail/DogDetail"
 
 class PageContainer extends Component {
 	render() {
 		return (
-			<div id="layout">
-				<Header />
-				<main>
-					<Switch>
-						<Route exact path="/" component={HomePage} />
-						{/* <Route path="/about" component={} /> */}
-						{/* <Route path="/contact" component={} /> */}
-					</Switch>
-				</main>
-				<Footer />
+			<div className="page-container">
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/games" component={Games} />
+					<Route exact path="/dog-list" component={DogList} />
+					<Route path="/dog-list/:breed" component={DogDetail} />
+				</Switch>
 			</div>
 		);
 	}
