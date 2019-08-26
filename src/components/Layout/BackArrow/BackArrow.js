8c99526fc0194af768de96b541de09f66d2bf903
msbb <mail@maximbuur.nl>
2019-08-26 16:52:01 +0200

    Working back arrow

--- src/components/Layout/BackArrow/BackArrow.js
+++ src/components/Layout/BackArrow/BackArrow.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
-import { Route, Link } from "react-router-dom"
-import BackArrowIcon from "../../../images/back.svg"
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import BackArrowIcon from "../../../images/back.svg";
 
 class BackArrow extends Component {
-    render() {
-        return <img className='backArrow' src={BackArrowIcon} alt="Back" />
-    }
+  goBack = () => {
+    this.props.history.goBack();
+  };
+
+  render() {
+    return (
+      <img
+        className="backArrow"
+        src={BackArrowIcon}
+        onClick={this.goBack}
+        alt="Back"
+      />
+    );
+  }
 }
 
-export default BackArrow
+export default withRouter(BackArrow);
