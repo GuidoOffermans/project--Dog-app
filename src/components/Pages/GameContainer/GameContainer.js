18855ea5a56aa7540b5a5de4f11d41d9f60d3235
GuidoOffermans <guidooffermans@hotmail.com>
2019-08-28 16:20:04 +0200

    game changes

--- src/components/Pages/GameContainer/GameContainer.js
+++ src/components/Pages/GameContainer/GameContainer.js
@@ -1,57 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { shuffleArray, chunkify } from './dogfunction';
 
-import Logo from '../../Layout/Logo/Logo';
-import BackArrow from '../../Layout/BackArrow/BackArrow';
-import HomeButton from '../../Layout/HomeButton/HomeButton';
-import Score from '../../GameComponents/Score/Score';
-import Hints from '../../GameComponents/Hints/Hints';
-import GetQuestion from '../../GameComponents/Question components/GetQuestion/GetQuestion';
+import GamePlayContainer from './GamePlayContainer';
 
 class GameContainer extends Component {
-	state = {
-		currentGameType: 'breed',
-		dogsCurrentlyInGame: [],
-		hasRightData: false
-	};
+  
+  componentDidMount() {
 
-	componentDidUpdate = () => {
-		if (
-			this.state.hasRightData === false &&
-			this.props.dogList.length > 0
-		) {
-			const chunkedDogs = chunkify(shuffleArray(this.props.dogList));
-			this.setState({
-				currentBreed: chunkedDogs[0][0],
-				dogsCurrentlyInGame: chunkedDogs,
-				hasRightData: true
-			});
-			// this.setState({ dogsCurrentlyInGame: chunkedDogs });
-			// this.setState({hasRightData: true})
-		}
-	};
+  }
 
-	render() {
-		return (
-			<div>
-				<header>
-					<BackArrow />
-					<HomeButton />
-					<Logo />
-					<Score />
-				</header>
-				<main>
-					<GetQuestion
-						dogsCurrentlyInGame={this.state.dogsCurrentlyInGame}
-						currentGameType={this.state.currentGameType}
-					/>
-				</main>
-				<footer>
-					<Hints />
-				</footer>
-			</div>
-		);
+  render() {
+		return this.props.dogList.length
+      ? <GamePlayContainer dogList={this.props.dogList} />
+      : 'Loading'
 	}
 }
 
