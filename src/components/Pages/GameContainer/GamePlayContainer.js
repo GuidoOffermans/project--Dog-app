6e5d015dac20a81551f25c7a18492d1fdaaafa19
andreeaCcss <andreeaccss@gmail.com>
2019-08-30 09:53:44 +0200

    hint bux fix

--- src/components/Pages/GameContainer/GamePlayContainer.js
+++ src/components/Pages/GameContainer/GamePlayContainer.js
@@ -1,99 +1,81 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
-	setCurrentBreed,
-	setGameType,
-	setNextQuestion,
-	addNextChunk,
-	clearDogPool
-} from '../../../redux/actions/gameActions';
-import { setScore } from '../../../redux/actions/scoreAction';
+  setCurrentBreed,
+  setGameType,
+  setNextQuestion,
+  addNextChunk,
+  clearDogPool
+} from "../../../redux/actions/gameActions";
+import { setScore } from "../../../redux/actions/scoreAction";
 import { showHint } from "../../../redux/actions/hintAction";
 
-import Logo from '../../Layout/Logo/Logo';
-import BackArrow from '../../Layout/BackArrow/BackArrow';
-import HomeButton from '../../Layout/HomeButton/HomeButton';
-import Score from '../../GameComponents/Score/Score';
-import Hints from '../../GameComponents/Hints/Hints';
-import GetQuestion from '../../GameComponents/Question components/GetQuestion/GetQuestion';
+import Logo from "../../Layout/Logo/Logo";
+import BackArrow from "../../Layout/BackArrow/BackArrow";
+import HomeButton from "../../Layout/HomeButton/HomeButton";
+import Score from "../../GameComponents/Score/Score";
+import Hints from "../../GameComponents/Hints/Hints";
+import GetQuestion from "../../GameComponents/Question components/GetQuestion/GetQuestion";
 
 class GameContainer extends Component {
-	state = {
-		previousGameType: ''
-	};
+  state = {
+    previousGameType: ""
+  };
 
-	componentDidMount = () => {
-		this.props.clearDogPool();
-		this.props.addNextChunk();
-		setTimeout(
-			() => this.props.setCurrentBreed(this.props.currentDogpool[0]),
-			100
-		);
-
-		this.props.setScore({
-			questionsAsked: 0,
-			correctAnswers: 0,
-			correctAnswersInARow: 0
-		});
-
-		if (this.props.gameType === 'mixed') {
-			const gameTypes = [ 'breed', 'picture' ];
-			const randomGameType =
-				gameTypes[Math.floor(Math.random() * gameTypes.length)];
-			this.setState({ previousGameType: randomGameType });
-		}
-
-		if (this.props.gameType === 'mixed') {
-			const gameTypes = [ 'breed', 'picture' ];
-			const randomGameType =
-				gameTypes[Math.floor(Math.random() * gameTypes.length)];
-			this.setState({ previousGameType: randomGameType });
-		}
-	};
-
-	runsEachRound = () => {
-     this.switchIfMixedGame();
+  componentDidMount = () => {
+    this.props.clearDogPool();
+    this.props.addNextChunk();
     this.props.showHint(false);
-		this.props.setNextQuestion(false);
-	};
+    setTimeout(
+      () => this.props.setCurrentBreed(this.props.currentDogpool[0]),
+      100
+    );
 
-	switchIfMixedGame = () => {
-		if (this.state.previousGameType === 'breed') {
-			this.setState({ previousGameType: 'picture' });
-			return 'picture';
-		} else {
-			this.setState({ previousGameType: 'breed' });
-			return 'breed';
-		}
-	};
+    this.props.setScore({
+      questionsAsked: 0,
+      correctAnswers: 0,
+      correctAnswersInARow: 0
+    });
 
-	render() {
-		if (this.props.onToNextQuestion) {
-			this.runsEachRound();
-		}
+    if (this.props.gameType === "mixed") {
+      const gameTypes = ["breed", "picture"];
+      const randomGameType =
+        gameTypes[Math.floor(Math.random() * gameTypes.length)];
+      this.setState({ previousGameType: randomGameType });
+    }
+  };
 
-		return (
-			<div>
-				<header>
-					<BackArrow />
-					<HomeButton />
-					<Logo />
-					<Score />
-				</header>
-				<main>
-					<GetQuestion
-						currentBreed={this.props.currentBreed}
-						gameType={
-							this.props.gameType === 'mixed' ? (
-								this.state.previousGameType
-							) : (
-								this.props.gameType
-							)
-						}
-					/>
-				</main>
-				<footer>
-					<Hints
+  runsEachRound = () => {
+    this.switchIfMixedGame();
+    this.props.showHint(false);
+    this.props.setNextQuestion(false);
+  };
+
+  switchIfMixedGame = () => {
+    if (this.state.previousGameType === "breed") {
+      this.setState({ previousGameType: "picture" });
+      return "picture";
+    } else {
+      this.setState({ previousGameType: "breed" });
+      return "breed";
+    }
+  };
+
+  render() {
+    if (this.props.onToNextQuestion) {
+      this.runsEachRound();
+    }
+
+    return (
+      <div>
+        <header>
+          <BackArrow />
+          <HomeButton />
+          <Logo />
+          <Score />
+        </header>
+        <main>
+          <GetQuestion
             currentBreed={this.props.currentBreed}
             gameType={
               this.props.gameType === "mixed"
@@ -101,28 +83,41 @@ class GameContainer extends Component {
                 : this.props.gameType
             }
           />
-				</footer>
-			</div>
-		);
-	}
+        </main>
+        <footer>
+          <Hints
+            currentBreed={this.props.currentBreed}
+            gameType={
+              this.props.gameType === "mixed"
+                ? this.state.previousGameType
+                : this.props.gameType
+            }
+          />
+        </footer>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		dogList: state.dogList,
-		chunkedDogs: state.game.chunkedDogs,
-		gameType: state.game.gameType,
-		onToNextQuestion: state.game.onToNextQuestion,
-		currentDogpool: state.game.currentDogpool
-	};
+const mapStateToProps = state => {
+  return {
+    dogList: state.dogList,
+    chunkedDogs: state.game.chunkedDogs,
+    gameType: state.game.gameType,
+    onToNextQuestion: state.game.onToNextQuestion,
+    currentDogpool: state.game.currentDogpool
+  };
 };
 
-export default connect(mapStateToProps, {
-	setCurrentBreed,
-	setGameType,
-	setNextQuestion,
-	setScore,
-	addNextChunk,
-	clearDogPool,
-  showHint
-})(GameContainer);
+export default connect(
+  mapStateToProps,
+  {
+    setCurrentBreed,
+    setGameType,
+    setNextQuestion,
+    setScore,
+    addNextChunk,
+    clearDogPool,
+    showHint
+  }
+)(GameContainer);
